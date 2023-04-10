f8d72c46ce0f3d9e132a2b5fdf104dd63b947d1d
StefanReljic <stefanreljic@hotmail.com>
2023-04-11 00:33:20 +0200

    Kafka stream serializer

--- src/main/java/com/yellow/eventmarket/kafka/consumer/MarketStreamConsumer.java
+++ src/main/java/com/yellow/eventmarket/kafka/consumer/MarketStreamConsumer.java
@@ -3,12 +3,13 @@ package com.yellow.eventmarket.kafka.consumer;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.stereotype.Component;
 
-import com.yellow.eventmarket.model.Market;
+import com.yellow.eventmarket.dto.MarketDTO;
 import com.yellow.eventmarket.service.MarketService;
 
+@Component
 public class MarketStreamConsumer {
 
 	@Autowired
@@ -20,8 +21,8 @@ public class MarketStreamConsumer {
 	@Value("${kafka.group.market}")
 	private String marketGroup;
 
-	@StreamListener(target = "market-topic")
-	public void processOfferMessage(@Input("market-topic") KStream<String, Market> offerStream) {
+	@StreamListener(value = "${kafka.topic.market}")
+	public void processOfferMessage(KStream<String, MarketDTO> offerStream) {
 		System.out.println("konzumiram kafku");
 		// Consume messages from the "create-offer-topic" topic
 		offerStream.foreach((key, value) -> {
